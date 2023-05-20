af6e267d1242f1935fc8bd5e33dfa571e5e53e82
hudadamar21 <hudadamar21@gmail.com>
2023-05-20 12:02:08 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./layouts/**/*.html", "./static/**/*.js"],
+  content: [
+    "./layouts/**/*.html", 
+    "./static/**/*.js", 
+    "./themes/**/*.{html,js}"
+  ],
   darkMode: "class",
   theme: {
     extend: {
-      
+      colors: {
+        primary: "#374151",
+        secondary: "#111827",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "0rem",
+          sm: "2rem",
+          md: "6rem",
+          lg: "10rem",
+          xl: "12rem",
+        },
+      },
+      fontFamily: {
+        "patrick-hand": "'Patrick Hand', cursive",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp"), require("@tailwindcss/typography")],
 };
